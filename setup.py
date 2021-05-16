0799c5b4009e2d7b4b2d0a51d32be7b106ddf4ca
Roman Kalivoda <kalivodaroman@gmail.com>
2021-05-16 12:39:31 +0200

    Update environment setup configuration

--- setup.py
+++ setup.py
@@ -3,12 +3,6 @@ import setuptools
 with open("requirements.txt", "r") as f:
     install_req = f.read()
 
-with open("requirements-dev.txt", "r") as f:
-    devel_req = f.read()
-
 setuptools.setup(
-    install_requires=install_req,
-    extras_require={
-        'dev': devel_req
-    }
+    install_requires=install_req
 )
