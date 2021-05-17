78ee4fe7a3ed0c1bb37a26961de647ea2dc8f374
Roman Kalivoda <kalivodaroman@gmail.com>
2021-05-17 10:16:49 +0200

    Revert "Update environment setup configuration"
    This reverts commit 0799c5b4

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@ import setuptools
 with open("requirements.txt", "r") as f:
     install_req = f.read()
 
+with open("requirements-dev.txt", "r") as f:
+    devel_req = f.read()
+
 setuptools.setup(
-    install_requires=install_req
+    install_requires=install_req,
+    extras_require={
+        'dev': devel_req
+    }
 )
